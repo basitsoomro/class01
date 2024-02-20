6de29b8122d80cfa9cb61b990571deec13c7282a
basitsoomro <160355095+basitsoomro@users.noreply.github.com>
2024-02-21 02:18:44 +0500

    Add files via upload

---
+++ class01/main.js
@@ -0,0 +1,5 @@
+// Question1
+console.log("hello world");
+// Question2 
+var message = ("this person is good");
+console.log(message);
---
+++ class01/main.ts
@@ -0,0 +1,6 @@
+// Question1
+console.log("hello world");
+
+// Question2 
+let message = ("this person is good");
+console.log(message)
